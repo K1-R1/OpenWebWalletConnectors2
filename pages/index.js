49636891a47312b7e29f53beed24f438221ab4f5
KR <kyranrawlinson@gmail.com>
2022-02-17 17:24:07 +0000

    Updated index; setup hone func

--- pages/index.js
+++ pages/index.js
@@ -3,3 +3,10 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [hasMetamask, setHasMetamask] = useState(false);
+  const [signer, setSigner] = useState(undefined);
+
+}

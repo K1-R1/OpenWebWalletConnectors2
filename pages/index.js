c214bdca8d693fbaf7ca11b681c027a87a4ec9ab
KR <kyranrawlinson@gmail.com>
2022-02-17 17:27:00 +0000

    Updated index; updated execute func with contract abi

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,108 @@ export default function Home() {
   async function execute() {
     if (typeof window.ethereum !== "undefined") {
       const contractAddress = "0x3Ac9d517Cf754Fcd4A1e9138D3e85389cC11c6d1";
-      const abi = ;
+      const abi = [
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "_name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_favouriteNumber",
+              "type": "uint256"
+            }
+          ],
+          "name": "addPerson",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "favouriteNumber",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "name": "nameToFavouriteNumber",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "people",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "favouriteNumber",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "retrieve",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "_favouriteNumber",
+              "type": "uint256"
+            }
+          ],
+          "name": "store",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        }
+      ];
       const contract = new ethers.Contract(contractAddress, abi, signer);
       try {
         await contract.store(42);
